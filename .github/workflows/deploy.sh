a58f6588eb8217649f240b6ed7a9c1dd4f102317
zhengshifa <84844958+zhengshifa@users.noreply.github.com>
2024-08-21 22:05:51 +0800

    test:v1

--- .github/workflows/deploy.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-for PLATFORM in $(echo $PLATFORMS | tr ',' '\n'); do
-  plat=${PLATFORM#*/}
-  echo "docker pull --platform ${PLATFORM} ${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${COMMIT_MESSAGE}_${plat}"| /usr/bin/bash
-done
