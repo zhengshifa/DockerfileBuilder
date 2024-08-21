9d272923c0b244ac802dc41489a2574b01682ccb
zhengshifa <84844958+zhengshifa@users.noreply.github.com>
2024-08-21 22:08:16 +0800

    test:v3

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ for PLATFORM in $(echo $PLATFORMS | tr ',' '\n'); do
   plat=${PLATFORM#*/}
   echo "docker pull --platform ${PLATFORM} ${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${COMMIT_MESSAGE}_${plat}" >> /tmp/test.txt
 done
+###
