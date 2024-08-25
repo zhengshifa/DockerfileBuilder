71b95d9b8a05492c1e08bb0b9fb25bc86e7064d0
zhengshifa <84844958+zhengshifa@users.noreply.github.com>
2024-08-25 17:35:42 +0800

    test:v2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-#/bin/bash
-echo $PLATFORMS >> /tmp/test.txt
+#!/bin/bash
+
 for PLATFORM in $(echo $PLATFORMS | tr ',' '\n'); do
-  plat=${PLATFORM#*/}
-  echo "docker pull --platform ${PLATFORM} ${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${COMMIT_MESSAGE}_${plat}" >> /tmp/test.txt
+  plat="${PLATFORM#*/}"
+  docker pull  --platform $PLATFORM  ${ALIYUN_REGISTRY}/${ALIYUN_NAME_SPACE}/${COMMIT_MESSAGE}_${plat}
 done
-###
